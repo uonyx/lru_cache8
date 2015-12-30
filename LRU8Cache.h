1cff3649e83028b01b68dbae7a158a0141797ea8
uonyx <ubakao@gmail.com>
2015-12-30 13:08:23 +0000

    code clean up and comments

--- LRU8Cache.h
+++ LRU8Cache.h
@@ -38,60 +38,52 @@
 // Is compiler is C++11 or newer?
 #define LRU8_CPP11 (__cplusplus >= 201103L) || (_MSC_VER >= 1600)
 
-// Use C++11 native hash functions which provides better support for STL data types 
-// but is platform-dependent
-#define LRU8_PREFER_CPP11_HASH (LRU8_CPP11 && 1)
+// Use C++11 native 'hash' and 'equal_to' functions as defaults. 
+// These provides better support for STL data types (but is platform-dependent).
+#define LRU8_PREFER_CPP11_FUNCTION_DEFAULTS (LRU8_CPP11 && 1)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-#include <string>
 #include <stdint.h>
-#if LRU8_CPP11
-#include <type_traits>
-#endif
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-#if LRU8_PREFER_CPP11_HASH
+#if LRU8_PREFER_CPP11_FUNCTION_DEFAULTS
 
 #include <functional>
+
 template<typename _Key, typename _Val, typename _KeyHash = std::hash<_Key>, typename _KeyEqual = std::equal_to<_Key> >
 
 #else
 
+#include <string>
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-uint32_t LRU8DJBX33X (const unsigned char *src, size_t sz)
+uint32_t LRU8Hash_DJBX33X (const unsigned char *src, size_t sz)
 {
   uint32_t hash = 5381;
-  size_t i = 0;
-  while (i < sz) 
-  {
-    unsigned char uc = src [i++];
-    hash = ((hash << 5) + hash) ^ uc;
-  }
+  for (size_t i = 0; i < sz; ++i) { hash = ((hash << 5) + hash) ^ src [i]; }
   return hash;
 }
 
-template<typename _Key>
-struct LRU8HashBitwise
+template<typename _Key> struct LRU8HashBitwise
 {
   uint32_t operator() (_Key k) const
   {
     const unsigned char *kd = reinterpret_cast<const unsigned char *>(&k);
     size_t sz = sizeof (_Key);
-    return LRU8DJBX33X (kd, sz);
+    return LRU8Hash_DJBX33X (kd, sz);
   }
 };
 
-template<typename _Key>
-struct LRU8HashNumeric
+template<typename _Key> struct LRU8HashNumeric
 {
   uint32_t operator() (_Key k) const
   {
@@ -100,20 +92,18 @@ struct LRU8HashNumeric
 };
 
 #if LRU8_CPP11
-template<typename _Key>
-struct LRU8Hash : public LRU8HashBitwise<_Key>
+#include <type_traits>
+template<typename _Key> struct LRU8Hash : public LRU8HashBitwise<_Key>
 {
   static_assert (std::is_enum<_Key>::value, "No default LRU8Hash available for this type");
 };
 #else
-template<typename _Key>
-struct LRU8Hash
+template<typename _Key> struct LRU8Hash
 {
 };
 #endif
 
-template<typename _Key>
-struct LRU8Hash<_Key *>
+template<typename _Key> struct LRU8Hash<_Key *>
 {
   uint32_t operator() (_Key *k) const
   {
@@ -122,49 +112,37 @@ struct LRU8Hash<_Key *>
   }
 };
 
-template<>
-struct LRU8Hash<char> : public LRU8HashNumeric<char> {};
+template<> struct LRU8Hash<char> : public LRU8HashNumeric<char> {};
 
-template<>
-struct LRU8Hash<unsigned char> : public LRU8HashNumeric<unsigned char> {};
+template<> struct LRU8Hash<unsigned char> : public LRU8HashNumeric<unsigned char> {};
 
-template<>
-struct LRU8Hash<short> : public LRU8HashNumeric<short> {};
+template<> struct LRU8Hash<short> : public LRU8HashNumeric<short> {};
 
-template<>
-struct LRU8Hash<unsigned short> : public LRU8HashNumeric<unsigned short> {};
+template<> struct LRU8Hash<unsigned short> : public LRU8HashNumeric<unsigned short> {};
 
-template<>
-struct LRU8Hash<int> : public LRU8HashNumeric<int> {};
+template<> struct LRU8Hash<int> : public LRU8HashNumeric<int> {};
 
-template<>
-struct LRU8Hash<unsigned int> : public LRU8HashNumeric<unsigned int> {};
+template<> struct LRU8Hash<unsigned int> : public LRU8HashNumeric<unsigned int> {};
 
-template<>
-struct LRU8Hash<long> : public LRU8HashBitwise<long> {};
+template<> struct LRU8Hash<long> : public LRU8HashBitwise<long> {};
 
-template<>
-struct LRU8Hash<unsigned long> : public LRU8HashBitwise<unsigned long> {};
+template<> struct LRU8Hash<unsigned long> : public LRU8HashBitwise<unsigned long> {};
 
-template<>
-struct LRU8Hash<long long> : public LRU8HashBitwise<long long> {};
+template<> struct LRU8Hash<long long> : public LRU8HashBitwise<long long> {};
 
-template<>
-struct LRU8Hash<unsigned long long> : public LRU8HashBitwise<unsigned long long> {};
+template<> struct LRU8Hash<unsigned long long> : public LRU8HashBitwise<unsigned long long> {};
 
-template<>
-struct LRU8Hash<std::string>
+template<> struct LRU8Hash<std::string>
 {
   uint32_t operator() (const std::string &k) const
   {
     const unsigned char *kd = reinterpret_cast<const unsigned char *>(k.c_str ());
     size_t sz = k.size ();
-    return LRU8DJBX33X (kd, sz);
+    return LRU8Hash_DJBX33X (kd, sz);
   }
 };
 
-template<typename _Key>
-struct LRU8Equal
+template<typename _Key> struct LRU8EqualTo
 {
   bool operator() (const _Key &k1, const _Key &k2) const
   {
@@ -176,7 +154,7 @@ struct LRU8Equal
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-template<typename _Key, typename _Val, typename _KeyHash = LRU8Hash<_Key>, typename _KeyEqual = LRU8Equal<_Key> >
+template<typename _Key, typename _Val, typename _KeyHash = LRU8Hash<_Key>, typename _KeyEqual = LRU8EqualTo<_Key> >
 
 #endif
 
@@ -296,6 +274,25 @@ private:
 
   void new_matrix ()
   {
+    /*
+      the low-order 8 bits hold row 0 of the matrix
+      the next 8 bits hold row 1, etc
+      byte i, i + 1, i + 2 etc
+
+      every 0-th bit of each byte holds column 0
+      the next byte's 0-th bit holds column 1
+      bits i, i + 8, i + 16, ... is set to 0
+
+      00000000
+      00000001
+      00000011
+      00000111
+      00001111
+      00011111
+      00111111
+      01111111
+    */
+
     uint64_t m = 0;
     uint8_t p = 0;
     for (uint8_t i = 1; i < MAX_SIZE; ++i)
@@ -316,6 +313,7 @@ private:
 
   uint8_t get_matrix_lru ()
   {
+    // search of an all-zero byte
     uint64_t m = m_matrix;
     if ((m & 0x00000000000000ff) == 0) return 0;
     if ((m & 0x000000000000ff00) == 0) return 1;
@@ -334,9 +332,11 @@ private:
 
   void set_matrix_mru (uint8_t i)
   {
+    // set row i (every bit of byte i) to 1s
     uint64_t rmask = 0xffull << (8 * i);
     m_matrix |= rmask;
 
+    // set column i (all i-th bits of each byte) to 0s     
     uint64_t cmask = ~(0x0101010101010101 << i);
     m_matrix &= cmask;
   }
